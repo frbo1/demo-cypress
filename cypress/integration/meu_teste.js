5e270ddd6a482b28befbd8bed437b6ad702e894e
Filipe <filipe.presentations@aleeas.com>
2021-08-09 19:41:22 -0300

    Feito

---
+++ cypress/integration/meu_teste.js
@@ -0,0 +1,13 @@
+// meu_teste.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Meu primeiro teste', () => {
+    it('Não faz nada', () => {
+      expect(true).to.equal(true)
+    })
+  })
+
